e272aa2ff5ba52d7b175bfcb88e2f1a7cb9bb7c6
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-17 20:09:27 -0300

    build: astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+  integrations: [tailwind()],
 });
