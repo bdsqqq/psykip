cf60d7ac74078532166f95e55604d8c6e65d6bce
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-18 12:56:01 -0300

    build: add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), prefetch()]
 });
