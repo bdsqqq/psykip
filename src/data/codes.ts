f3240dfe82ed66fa02a6b908d95f8beddf6dec8c
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-25 23:44:27 -0300

    feat: add translatorNamesByCode

--- src/data/codes.ts
+++ src/data/codes.ts
@@ -1,1 +1,13 @@
 export default ["e", "ec", "gl", "pem", "sw", "twh", "twr", "wao"] as const;
+
+export const translatorNamesByCode = {
+  e: "Epictetus",
+  ec: "Elizabeth Carter",
+  gl: "George Long",
+  pem: "P.E. Matheson",
+  sw: "Stephen Walton",
+  twh: "T.W. Higginson",
+  twr: "T.W. Rolleston",
+  wao: "William Abbott Oldfather",
+} as const;
+export type TranslationCode = keyof typeof translatorNamesByCode;
