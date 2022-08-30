13f8eb24c1010c9ee34f3a78fd72b45787f8e5ef
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-30 11:57:47 -0300

    build: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import { astroImageTools } from "astro-imagetools";
-
+import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 
 // https://astro.build/config
@@ -12,5 +12,6 @@ export default defineConfig({
       external: ["svgo"],
     },
   },
-  integrations: [tailwind(), prefetch(), solid(), astroImageTools],
+  site: "https://psykip.vercel.app",
+  integrations: [tailwind(), prefetch(), solid(), astroImageTools, sitemap()],
 });
