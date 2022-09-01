7ad72ba3b76323e42efc6eb935a755baea4c825e
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-31 23:09:32 -0300

    build: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
   site: "https://psykip.vercel.app",
-  integrations: [tailwind(), prefetch(), solid(), astroImageTools, sitemap()],
+  integrations: [tailwind(), prefetch(), solid(), astroImageTools, sitemap(), partytown()]
 });
