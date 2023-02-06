54eefd91404aa141123855979fc5857b0213dc2d
Igor Bedesqui <igorbedesqui@gmail.com>
2023-02-06 20:26:58 +0000

    build: upgrade to astro 2.0; update content collections methods from beta counterparts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
 import { z, defineCollection } from "astro:content";
-import codes, { TranslationCode } from "../data/codes";
+import codes from "../data/codes";
 
 const translations = defineCollection({
-  schema: {
+  schema: z.object({
     Author: z.string(),
     Title: z.string(),
     Translator: z.string(),
@@ -12,8 +12,8 @@ const translations = defineCollection({
       url: z.string(),
     }),
     Code: z.enum(codes),
-    Chapter: z.number(),
-  },
+    Chapter: z.number().int().min(1).max(53),
+  }),
 });
 
 export const collections = {
