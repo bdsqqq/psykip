0b1a1dd9f44ed7216c1aed32df778a97c43834cf
Igor Bedesqui <igorbedesqui@gmail.com>
2024-04-29 20:53:52 +0100

    build: upgrade everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import prefetch from "@astrojs/prefetch";
 import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 import partytown from "@astrojs/partytown";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
   site: "https://psykip.vercel.app",
-  integrations: [
-    prefetch(),
-    solid(),
-    astroImageTools,
-    sitemap(),
-    partytown(),
-    tailwind(),
-  ],
+  integrations: [solid(), astroImageTools, sitemap(), partytown(), tailwind(), solidJs()]
 });
