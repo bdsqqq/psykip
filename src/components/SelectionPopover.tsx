66380bd6761e96a6d378e504ba157ec3a87471ac
Igor Bedesqui <igorbedesqui@gmail.com>
2024-05-05 23:43:24 +0100

    feat: hover states to bookmarks, add trash button

--- src/components/SelectionPopover.tsx
+++ src/components/SelectionPopover.tsx
@@ -1,6 +1,6 @@
 import * as Ariakit from '@ariakit/react';
-import { useEffect, useRef } from 'react';
-import { BookmarkIcon, BookmarkFilledIcon, ClipboardCopyIcon, Share2Icon } from '@radix-ui/react-icons';
+import { useEffect, useRef, useState } from 'react';
+import { BookmarkIcon, BookmarkFilledIcon, ClipboardCopyIcon, Share2Icon, TrashIcon } from '@radix-ui/react-icons';
 import { translatorNamesByCode } from '../data/codes';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { withQueryClient } from './TanstackQuery';
@@ -279,23 +279,28 @@ export const BookmarksList = withQueryClient(() => {
   )
 
   return (
-    <ul className='flex flex-col gap-4'>
+    <ul className='flex flex-col gap-6 focus-within:text-zinc-400 hover:text-zinc-400 dark:focus-within:text-zinc-600 dark:hover:text-zinc-600 transition-colors pointer-events-none'>
       {bookmarks.map((bookmark) => (
-        <li key={bookmark.id}>
+        <li key={bookmark.id} className='flex gap-1 w-full justify-between items-start focus-within:text-zinc-900 hover:text-zinc-900 dark:focus-within:text-zinc-100 dark:hover:text-zinc-100 transition-colors pointer-events-auto'>
           <a href={`/read/${bookmark.translationCode}/${bookmark.chapterStart}`}>
             <div className='flex items-center gap-2'>
             {/* @ts-ignore - I know the translation code will be valid */}
             <span className='font-semibold'>{translatorNamesByCode[bookmark.translationCode]}</span>
             {
               bookmark.chapterStart !== bookmark.chapterEnd ? (
-                <span className='text-zinc-500 dark:text-zinc-400'>
+                <span className='opacity-30'>
                   {bookmark.chapterStart}-{bookmark.chapterEnd}
                 </span>
-              ) : <span className='text-zinc-500 dark:text-zinc-400'>{bookmark.chapterStart}</span>
+              ) : <span className='opacity-30'>{bookmark.chapterStart}</span>
             }
             </div>
             <p className='line-clamp-2'>{bookmark.text}</p>
           </a>
+          <button className='opacity-30 hover:opacity-100 focus:opacity-100 transition-opacity' onClick={() => {
+            // removeBookmark(bookmark.id);
+          }}>
+            <TrashIcon />
+          </button>
         </li>
       ))}
     </ul>
@@ -306,17 +311,24 @@ export const BookmarksList = withQueryClient(() => {
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { newId } from '../lib/id';
 
-export const Sheet = ({ children }: { children: React.ReactNode }) => {
-  
+export const Sheet = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
+
   return (
-    <Collapsible.Root className='bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-200 w-min data-[state="open"]:w-full md:data-[state=open]:w-96 animate-in slide-in-from-bottom-8 rounded-t-lg overflow-auto shadow-md border border-zinc-300 dark:border-zinc-700 border-b-zinc-100 dark:border-b-zinc-900 max-h-[70svh]'>
-      <Collapsible.Trigger className='sticky top-0 bg-zinc-100 dark:bg-zinc-900 p-1 px-1.5 w-full flex justify-between items-center border-b border-zinc-300 dark:border-zinc-700'>
+    <Collapsible.Root
+      open={isOpen}
+     className='bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-200 w-min data-[state="open"]:w-full md:data-[state=open]:w-96 animate-in slide-in-from-bottom-8 rounded-t-lg overflow-auto shadow-md border border-zinc-300 dark:border-zinc-700 border-b-zinc-100 dark:border-b-zinc-900 max-h-[70svh]'>
+      <Collapsible.Trigger onClick={() => {
+        !isOpen && open();
+      }} className='sticky top-0 bg-zinc-100 dark:bg-zinc-900 p-1 px-1.5 w-full flex justify-between items-center border-b border-zinc-300 dark:border-zinc-700'>
         <span className='flex items-center gap-1'>
           <BookmarkFilledIcon /> Bookmarks
         </span>
       </Collapsible.Trigger>
       <Collapsible.Content className='p-2 w-full'>
-        {children}
+        <BookmarksList/>
       </Collapsible.Content>
     </Collapsible.Root>
   )
