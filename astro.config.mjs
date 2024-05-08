edae09e7089c6dec2b6f8ea2dbc4442fc572c3aa
Igor Bedesqui <igorbedesqui@gmail.com>
2024-05-08 14:17:16 -0300

    build: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,19 @@ import { astroImageTools } from "astro-imagetools";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    ssr: {      
+    ssr: {
       external: ["svgo"]
     }
   },
   site: "https://psykip.vercel.app",
   integrations: [astroImageTools, sitemap(), partytown(), tailwind(), react()],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel()
 });
