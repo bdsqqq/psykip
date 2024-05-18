0e56626fe3b0c5bb4b7ae0d12b68bc3cd60ec29e
Igor Bedesqui <igorbedesqui@gmail.com>
2024-05-17 21:35:31 -0300

    docs(login): todo from no internet

---
+++ src/pages/api/login.ts
@@ -0,0 +1,3 @@
+// Igor, you were lookin at `https://github.com/ugurkellecioglu/next-14-lucia-auth-postgresql-drizzle-typescript-example/blob/magic-link/actions/magic-link.actions.ts`
+// you were about to implement sign-in, but you realized the person has a "generate jwt" function that links to `api/auth/magic-link`, and you can't really see the logic in that page without internet sadly.
+
